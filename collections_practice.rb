01c9f5f09eab58e81fd8e65cf5affe628523aa8c
Ted Sazon <tedsazon@gmail.com>
2018-12-19 03:42:57 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,49 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |x, y|
+    y <=> x
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |x, y|
+    x.length <=> y.length
+  end
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+  array.reverse.to_a
+end
+
+def kesha_maker(array)
+  array.map do |name|
+    name.sub(name[2], "$")
+  end
+end
+
+def find_a(array)
+  array.select do |name|
+    name.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject(0, &:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect do |name, index|
+    if index != 1
+      name << "s"
+    else
+      name
+    end
+  end
+end
